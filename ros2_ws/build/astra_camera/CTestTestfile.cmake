8d12855a12757dc5733cf01145b51acaeb6c030c
dayangyouxia <13466213+dayangyouxia222@user.noreply.gitee.com>
2025-05-19 13:50:44 +0000

    删除文件 ros2_ws

--- ros2_ws/build/astra_camera/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bingda/ros2_ws/src/hardware_driver/camera/ros2_astra_camera/astra_camera
-# Build directory: /home/bingda/ros2_ws/build/astra_camera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros2_ws/src/bingda_ros2_demos/robot_navigation_ros2/launch/lidar/cp210x_lidar_udev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="10c4", MODE:="0777", GROUP:="dialout",  SYMLINK+="lidar"' >/etc/udev/rules.d/lidar.rules
-
-service udev reload
-sleep 1
-service udev restart
